720a190ebbdd7df4569fffbb130ea486011608fd
ezeilo-su <ezeilo.su@gmail.com>
2020-12-04 23:48:41 +0100

    Change constructor func to factory func. Make showLib func an IIFE.

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -5,17 +5,18 @@ const tinput = document.querySelector('.b-title');
 const ainput = document.querySelector('.b-author');
 const ninput = document.querySelector('.b-pages');
 
-function Book(title, author, numpages, read) {
-  this.title = title;
-  this.author = author;
-  this.numpages = numpages;
-  this.read = read;
-}
+const bookCreate = (bookTitle, bookAuthor, bookNumOfPages, bookRead) => {
+  let title = bookTitle;
+  let author = bookAuthor;
+  let numOfPages = bookNumOfPages;
+  let read = bookRead;
 
+  return { title, author, numOfPages, read };
+};
 
 // Tests -------------------------
-const book1 = new Book('may', 'sunday', '500', 'Not Read');
-const book2 = new Book('december', 'jurgen', '300', 'Read');
+const book1 = bookCreate('may', 'sunday', '500', 'Not Read');
+const book2 = bookCreate('december', 'jurgen', '300', 'Read');
 
 library.push(book1);
 library.push(book2);
@@ -24,49 +25,54 @@ function showForm() {
   form.className = 'show';
 }
 
-function showLibrary() {
-  libWrap.innerHTML = '';
-  for (let i = 0; i < library.length; i += 1) {
-    const bookCard = document.createElement('div');
-    const btitle = document.createElement('h2');
-    const asubtitle = document.createElement('h3');
-    const content = document.createElement('p');
-    btitle.innerText = library[i].title;
-    asubtitle.innerText = `Author: ${library[i].author.toUpperCase()}`;
-    content.innerHTML = `Pages: ${library[i].numpages} ${library[i].read}`;
-
-    bookCard.setAttribute('class', 'book-card');
-    btitle.setAttribute('class', 'card-title');
-    asubtitle.setAttribute('class', 'card-author');
-    content.classList = 'card-content';
-
-    const rbtn = document.createElement('button');
-    rbtn.innerHTML = 'Delete Book';
-    rbtn.id = `remove-${i}`;
-    rbtn.className = 'btn';
-    rbtn.setAttribute('onclick', `deleteBook(${i})`);
-    const sbtn = document.createElement('button');
-    sbtn.innerHTML = 'Status';
-    sbtn.id = `status-${i}`;
-    sbtn.className = 'btn';
-    sbtn.setAttribute('onclick', `readStatus(${i})`);
-    bookCard.appendChild(btitle);
-    bookCard.appendChild(asubtitle);
-    bookCard.appendChild(content);
-    bookCard.appendChild(rbtn);
-    bookCard.appendChild(sbtn);
-    libWrap.appendChild(bookCard);
+const myModule = (() => {
+  const appendContent = (wrap, children) => {
+    children.forEach(child => {
+      wrap.appendChild(child);
+    });
   }
-}
+
+  const showLibrary = () => {
+    libWrap.innerHTML = '';
+    for (let i = 0; i < library.length; i += 1) {
+      const bookCard = document.createElement('div');
+      const btitle = document.createElement('h2');
+      const asubtitle = document.createElement('h3');
+      const content = document.createElement('p');
+      btitle.innerText = library[i].title;
+      asubtitle.innerText = `Author: ${library[i].author.toUpperCase()}`;
+      content.innerHTML = `Pages: ${library[i].numOfPages} ${library[i].read}`;
+  
+      bookCard.setAttribute('class', 'book-card');
+      btitle.setAttribute('class', 'card-title');
+      asubtitle.setAttribute('class', 'card-author');
+      content.classList = 'card-content';
+  
+      const rbtn = document.createElement('button');
+      rbtn.innerHTML = 'Delete Book';
+      rbtn.id = `remove-${i}`;
+      rbtn.className = 'btn';
+      rbtn.setAttribute('onclick', `deleteBook(${i})`);
+      const sbtn = document.createElement('button');
+      sbtn.innerHTML = 'Status';
+      sbtn.id = `status-${i}`;
+      sbtn.className = 'btn';
+      sbtn.setAttribute('onclick', `readStatus(${i})`);
+      appendContent(bookCard, [ btitle, asubtitle, content, rbtn, sbtn ]);
+      libWrap.appendChild(bookCard);
+    }
+  }
+  return { showLibrary };
+})();
 
 function addBookToLibrary() {
   if (tinput.value === '' || ainput.value === '' || ninput.value === '') {
     alert('All form fields must be filled!');
   } else {
-    const book = new Book(tinput.value, ainput.value, ninput.value);
+    const book = bookCreate(tinput.value, ainput.value, ninput.value);
     library.push(book);
     form.className = 'hide';
-    showLibrary();
+    myModule.showLibrary();
   }
 }
 
@@ -76,12 +82,12 @@ function readStatus(b) {
   } else {
     library[b].read = 'Read';
   }
-  showLibrary();
+  myModule.showLibrary();
 }
 
 function deleteBook(idx) {
   library.splice(idx, 1);
-  showLibrary();
+  myModule.showLibrary();
 }
 
-showLibrary();
+myModule.showLibrary();
